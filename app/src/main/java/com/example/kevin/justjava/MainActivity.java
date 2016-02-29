b2e87febf77c1f624e95aa78c2e15df6d34ed966
Kevin Parnell <kpbballer@gmail.com>
2016-02-29 11:42:58 -0500

    Cleaner code

--- app/src/main/java/com/example/kevin/justjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/justjava/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         int price = quantity * 5;
         String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "                                                                   Thank you!";
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
     }
     /**
